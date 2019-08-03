99f502aa552960252524406ddc826a523d0e49e2
JackM400 <jack.millar400@gmail.com>
2019-08-03 15:04:35 +0100

    user input

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,8 +1,8 @@
 import string
 
 print(help(string))
-print(string.ascii_letters)
-print(string.ascii_lowercase)
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
 
 import random
 
@@ -17,8 +17,21 @@ def generator():
     letter4 = random.choice(string.ascii_lowercase)
     letter5 = random.choice(string.ascii_lowercase)
     letter6 = random.choice(string.ascii_lowercase)
-    name1 = letter1 + letter2 + letter3 + letter4 + letter5 + letter6
-    return(name)
+    name = letter1 + letter2 + letter3 + letter4 + letter5 + letter6
+    return (name)
 
 
 print(generator())
+letter = string.ascii_lowercase
+vowels = 'aeiou'
+consonants = string.ascii_lowercase - vowels
+
+# user input
+letter_input1 = input('Choose a letter \n\t"v" for vowels,\n\t"c" for constants ,\n\t"l" for any other letter\n')
+letter_input2 = input('Choose a letter \n\t"v" for vowels,\n\t"c" for constants ,\n\t"l" for any other letter\n')
+letter_input3 = input('Choose a letter \n\t"v" for vowels,\n\t"c" for constants ,\n\t"l" for any other letter\n')
+letter_input4 = input('Choose a letter \n\t"v" for vowels,\n\t"c" for constants ,\n\t"l" for any other letter\n')
+letter_input5 = input('Choose a letter \n\t"v" for vowels,\n\t"c" for constants ,\n\t"l" for any other letter\n')
+
+# conditional logic
+
