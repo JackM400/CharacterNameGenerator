1cc41034e7dce2c44da81d62a5e5511610a14793
JackM400 <jack.millar400@gmail.com>
2019-08-03 14:33:15 +0100

    random name generator name is built from set of random characters[lowercase] no logic built

---
+++ NameGenerator.py
@@ -0,0 +1,24 @@
+import string
+
+print(help(string))
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+
+import random
+
+print(random.choice('pull a char from here'))
+print(random.choice(string.ascii_lowercase + '-'))
+
+
+def generator():
+    letter1 = random.choice(string.ascii_lowercase)
+    letter2 = random.choice(string.ascii_lowercase)
+    letter3 = random.choice(string.ascii_lowercase)
+    letter4 = random.choice(string.ascii_lowercase)
+    letter5 = random.choice(string.ascii_lowercase)
+    letter6 = random.choice(string.ascii_lowercase)
+    name = letter1 + letter2 + letter3 + letter4 + letter5 + letter6
+    return(name)
+
+
+print(generator())
